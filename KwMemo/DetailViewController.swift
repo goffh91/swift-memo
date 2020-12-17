1933a3825ab9d2292e7c6efe3cbd9c05459cf38a
goffh91 <goffh91@naver.com>
2020-12-17 21:11:57 +0900

    memo detail view

--- KwMemo/DetailViewController.swift
+++ KwMemo/DetailViewController.swift
@@ -14,3 +14,28 @@ class DetailViewController: UIViewController {
     }
 
 }
+
+extension DetailViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Memo.dummyMemoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
+        
+            return cell
+            
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+            
+            return cell
+    
+        default:
+            fatalError()
+        }
+    }
+    
+}
