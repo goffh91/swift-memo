21199353b2e41c1edb7a00a9edc1bd36b48c5d48
goffh91 <goffh91@naver.com>
2020-12-20 12:23:12 +0900

    add action of cancel attempt

--- KwMemo/ComposeViewController.swift
+++ KwMemo/ComposeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ComposeViewController: UIViewController {
     
     var editTarget: Memo?
+    var originalMemoContent: String?
     
     @IBOutlet weak var textView: UITextView!
     
@@ -19,14 +20,30 @@ class ComposeViewController: UIViewController {
         if let memo = editTarget {
             navigationItem.title = "메모 편집"
             textView.text = memo.content
+            originalMemoContent = memo.content
         } else {
             navigationItem.title = "새 메모"
             textView.text = ""
         }
+        
+        textView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.presentationController?.delegate = self
     }
     
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+        
+        navigationController?.presentationController?.delegate = nil
     }
     
     @IBAction func save(_ sender: Any) {
@@ -49,6 +66,38 @@ class ComposeViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+}
+
+extension ComposeViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        if let original = originalMemoContent, let edited = textView.text {
+            if #available(iOS 13.0, *) {
+                isModalInPresentation = original != edited
+            } else {
+                // Fallback on earlier versions
+            }
+        }
+    }
+}
+
+extension ComposeViewController: UIAdaptivePresentationControllerDelegate {
+    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+        
+        let alert = UIAlertController(title: "알림", message: "편집한 내용을 저장할까요?", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "확인", style: .default) {
+            [weak self] (action) in self?.save(action)
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel) {
+            [weak self] (action) in self?.close(action)
+        }
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
 }
 
 extension ComposeViewController {
